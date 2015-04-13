d82a3bf5b691455cfd2ba27e6a00d5a12f5fa4b1
Tyler Cloke <tylercloke@gmail.com>
2015-04-12 22:56:32 -0700

    Updated README.

--- README.md
+++ README.md
@@ -1,19 +1,68 @@
 # lita-github-team-review
 
-TODO: Add a description of the plugin.
-
-## Installation
-
-Add lita-github-team-review to your Lita instance's Gemfile:
-
-``` ruby
-gem "lita-github-team-review"
-```
-
-## Configuration
-
-TODO: Describe any configuration attributes the plugin exposes.
+This is a lita handler for listing pull requests and
+issues that are in need of review for a team.
 
 ## Usage
 
-TODO: Describe the plugin's features and how to use them.
+### In Hipchat
+
+```
+botname review <team>
+```
+
+### In Github
+
+Simply assign your team's github review user to a PR
+or issue on github to have the bot pick it up. These
+users must be configured separately and follow a
+specific format (see below). For example, the legion
+of builds team has the github user `lob-review`.
+`botname review lob` will properly return all PRs
+that that user is assigned to.
+
+### Implementation
+
+Due to the limited nature of the github API, looking
+up all pull requests for a team or organization is not
+possible. It would be possible to find all users for a
+team and list their PRs, but you have to be auth'ed
+as that user.
+
+Instead, we simply create a new user of the format
+`<team_name>-review` in github, and deploy that user's
+credentials in the root dir of the repo under a file
+named `<team_name>-review-config.yml`. The bot will
+parse any yml files of that format and use the creds
+to auth as that user and find all the issues it
+belongs to. So a call to `botname review lob` will
+load `lob-review-config.yml` from the root of the
+repo (so we'll need to deploy that with Chef) and
+use its contents to auth against the github API as
+that user, finding all PRs its assigne of.
+
+See example.yml for format.
+
+### Setting Up A New Team
+
+1. Create a new github user named `<team_name>-review`.
+2. Have someone with privileges invite that user to the Chef organization with read access (if you want to be able to find private repos needing review).
+3. In the deploy cookbook, deploy `<team_name>-review-config.yml` to the root of the installed repo with the proper values (see example.yml).
+4. Deploy the cookbook.
+5. Assign `<team_name>-review` to github issues and pull requests that need review from your team.
+
+Upon doing the above, you can now call
+
+```
+@julia review <team_name>
+```
+
+to list all pull requests needing review.
+
+Happy reviewing!
+
+### TODO
+
++ Tests
++ Better hipchat output
++ Periodicly output in team rooms?
--- lib/lita/handlers/github_team_review.rb
+++ lib/lita/handlers/github_team_review.rb
@@ -1,68 +1,48 @@
 require 'octokit'
 require 'yaml'
-require 'pp'
 
 module Lita
   module Handlers
     class GithubTeamReview < Handler
 
-      # TODO update help
       route(/^review\s+(.+)/, :review, command: true, help: {
-        "review GITHUB_TEAM" => "Responds with all PRs that need review for a github team."
+        "review GITHUB_TEAM" => "Responds with all issues that need review for a github team."
       })
 
       attr_reader :client
 
       def review(response)
-          config = YAML.load_file('config.yml')
+        team_arg = response.args[0]
+        if response.args.length != 1
+          response.reply "The review command only takes one argument."
+          response.reply "Please pass a valid github team as a string."
+        end
+
+        begin
+          config = YAML.load_file("#{team_arg}-review-config.yml")
           @client = Octokit::Client.new(:login => config["login"], :password => config["password"])
-          team_arg = response.args[0]
-          if response.args.length != 1
-            response.reply "The review command only takes one argument."
-            response.reply "Please pass a valid github team as a string."
+        rescue Errno::ENOENT => ex
+          response.reply "The team you passed (#{team_arg}) is yet configured for the review command."
+          response.reply "Valid teams currently are:"
+          Dir["./*.yml"].each do |team_yml_file|
+            # strip ./ and -review-config.yml
+            response.reply team_yml_file[2..-19]
           end
-
-          # get all chef teams and exit if the requested team doesn't exist
-          result = get_team(team_arg)
-          team = result[0]
-          teams = result[1]
-          if team.nil?
-            response.reply "The team you passed (#{team_arg}) does not exist in the Chef org."
-            response.reply "Valid teams are:"
-            teams.each do |valid_teams|
-              response.reply valid_teams.slug
-            end
-            return nil
-          end
-
-          get_pull_requests_that_need_review(team)
-
-      end
-
-      private
-
-      def get_team(team_arg)
-        teams = @client.organization_teams("chef")
-        wanted_team = nil
-        teams.each do |team|
-          wanted_team = team if team.slug == team_arg
-        end
-        [wanted_team, teams]
-      end
-
-      def get_pull_requests_that_need_review(team)
-        puts "Halp"
-        #@client.user.login
-        #Octokit.auto_paginate = true
-        #puts issues @client.org_issues("chef")
-
-        puts @client.list_issues.inspect
-        @client.team_repositories(team.id).each do |team_repo|
-          #puts @client.pull_requests(team_repo.id, :state => 'open')[0].inspect
+          return nil
         end
 
-        @client.team_members(team.id).each do |member|
+        issues = @client.list_issues
+        if issues.length == 0
+          response.reply "There are currently no issues or pull requests to review for team #{team_arg}!"
+          response.reply "To mark an issue or pull request as needing review, assign it to the github user #{team_arg}-review."
+        else
+          response.reply "There are #{issues.length} issues and pull requests currently in need of review for team #{team_arg}.\n"
+        end
 
+        issues.each do |issue|
+          response.reply issue[:title]
+          response.reply "Author: #{issue[:user][:login]}"
+          response.reply "Url:    #{issue[:html_url]}\n"
         end
       end
     end
